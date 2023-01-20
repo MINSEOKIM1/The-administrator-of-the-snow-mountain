50ef00ba8abdc78cd15ee4cc588e4b26ca976bb2
jych <jych1109@gmail.com>
2023-01-20 20:15:18 +0900

    reduceScriptableObjects
    스크립터블 오브젝트 줄였습니다

---
+++ Assets/Scripts/Data/VillageData.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[Serializable]
+public class VillageData
+{
+    public string name;
+    public int maxMob;
+    public int curMob;
+}
--- Assets/Scripts/ScriptableObject/RespawnTimeInfo.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-[CreateAssetMenu(fileName = "SceneInfo", menuName = "ScriptableObjects/RespawnTimeInfo", order = 1)]
-public class RespawnTimeInfo : ScriptableObject
-{
-    public float[] respawnTime;
-}
