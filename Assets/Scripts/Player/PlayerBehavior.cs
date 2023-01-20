b77ecc657d455d6c8d304bab6758e8da3efd4e3f
Minseo Jeon <minseo4565@snu.ac.kr>
2023-01-20 21:00:00 +0900

    Add Wall Climbing, Wall Jump

--- Assets/Scripts/Player/PlayerBehavior.cs
+++ Assets/Scripts/Player/PlayerBehavior.cs
@@ -10,17 +10,22 @@ using Vector3 = UnityEngine.Vector3;
 public class PlayerBehavior : Entity
 {
     // Player gameObject's components
+    [SerializeField] private Transform wallCheckPos;
     private PlayerInputHandler _playerInputHandler;
     private PlayerAttack _playerAttack;
-
+    
     // for below variable, public will be private ... (for debuging, it is public now)
     // for record current state
+    public bool isClimb;
     
 
     // tmp (will be removed maybe...)
     public float dashElapsed;
+    public float wallJump;
+    public float wallJumpPower;
     
     // tmp variable (for avoiding creating a new object to set value like _rigid.velocity, _graphic.localScale)
+    public float leftDis;
 
     // input detect variable
     public bool _normalAttackDetect;
@@ -46,7 +51,8 @@ public class PlayerBehavior : Entity
     protected override void FixedUpdate()
     {
         ApplyAnimation();
-        CheckGround();                      
+        CheckGround();
+        CheckWall();
         UpdateVelocity();
         AttackInputDetect();
         UpdateExternalVelocity();
@@ -71,6 +77,59 @@ public class PlayerBehavior : Entity
      * 2. Slope?
      */
     // CheckGround() is inherited... and no override
+    
+    
+    /*
+     * Check if...
+     * 1. isClimb?
+     */
+    private void CheckWall()
+    {
+        var hits = Physics2D.RaycastAll(
+            wallCheckPos.position,
+            Vector2.left * playerGraphicTransform.localScale.x,
+            leftDis
+        );
+        
+        Debug.DrawRay(wallCheckPos.position,
+            Vector2.left * playerGraphicTransform.localScale.x * leftDis* playerGraphicTransform.localScale.x,
+            Color.black
+            );
+
+        foreach (var hit in hits)
+        {
+            if (hit)
+            {
+                if (hit.collider.gameObject.CompareTag("Ground"))
+                {
+                    if (!isClimb &&
+                        (int)_playerInputHandler.movement.x == -(int)playerGraphicTransform.localScale.x &&
+                        !_isGround &&
+                        !_isAttack &&
+                        hit.collider.gameObject.GetComponent<PlatformEffector2D>() == null &&
+                        wallJump <= 0 &&
+                        _rigidbody.velocity.y < 0)
+                    {
+                        _canJump = true;
+                        isClimb = true;
+                        _speed = 0;
+                    }
+                    else
+                    {
+                        isClimb = false;
+                    }
+
+                    break;
+                }
+                else
+                {
+                    isClimb = false;
+                }
+            }
+        }
+        
+        _animator.SetBool("climb", isClimb);
+    }
 
     /*
      * according to Input...
@@ -81,7 +140,7 @@ public class PlayerBehavior : Entity
         // if condition (!_inSlope) is omitted... player slide down a slope...
         if (!_inSlope || (_inSlope && Mathf.Abs(dashSpeed) > 1))
         {
-            _speed = _rigidbody.velocity.x;
+            if (wallJump<0) _speed = _rigidbody.velocity.x;
         }
 
         if (_playerInputHandler.movement.x != 0 && !_isAttack && !_hitAir)
@@ -128,11 +187,31 @@ public class PlayerBehavior : Entity
      * according to _speed, playerDashSpeed, externalSpeed, ground checking condition...
      * update rigid.velocity -> apply change on real player's movement;
      */
-    // Method Move() is inherited from Entity class, and no override
+    protected override void Move(Vector2 direction, float speed)
+    {
+        wallJump -= Time.fixedDeltaTime;
+        base.Move(direction, speed);
+        if (isClimb && wallJump <= 0) _rigidbody.velocity = new Vector2(_rigidbody.velocity.x, 0);
+    }
 
     public void Attack()
     {
         _normalAttackDetect = true;
+    }
+
+    public override void Jump()
+    {
+        if (!isClimb)
+        {
+            base.Jump();
+        }
+        else
+        {
+            wallJump = 0.3f;
+            _speed = maxSpeed * (_graphicLocalScale.x);
+            dashSpeed = wallJumpPower * (_graphicLocalScale.x);
+            base.Jump();
+        }
     }
 
     public void KnockBack()
@@ -174,5 +253,21 @@ public class PlayerBehavior : Entity
         yield return new WaitForSecondsRealtime(0.2f);
 
         Time.timeScale = 1;
+    }
+    
+    protected override void OnCollisionStay2D(Collision2D col)
+    {
+        if (col.gameObject.tag.Equals("Ground"))
+        {
+            if (col.contacts[0].normal.y > 0.7) CheckGround();
+            else
+            {
+                if (Mathf.Abs(col.contacts[0].normal.x) == 1 && wallJump < 0)
+                {
+                    externalSpeed = 0;
+                    dashSpeed = 0;
+                }
+            }
+        }
     }
 }
